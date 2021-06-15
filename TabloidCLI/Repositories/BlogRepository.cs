b4ff4ff450f32a5bcafae553915de2378fcf2337
Dakota <dakota.c.upchurch@gmail.com>
2021-06-15 14:18:52 -0500

    save point

---
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI
+{
+    public class BlogRepository : DatabaseConnector, IRepository<Blog>
+    {
+        public BlogRepository(string connectionString) : base(connectionString) { }
+
+        public List<Blog> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id,
+                                               Title,
+                                               Url
+                                          FROM Blog";
+
+                    List<Blog> blogs = new List<Blog>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Blog blog = new Blog()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Url = reader.GetString(reader.GetOrdinal("Url")),
+                        };
+                        blogs.Add(blog);
+                    }
+
+                    reader.Close();
+
+                    return blogs;
+                }
+            }
+        }
+
+        public Blog Get(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT b.Id AS BlogId,
+                                               b.Title,
+                                               b.Url
+                                        FROM Blog b
+                                        WHERE b.Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    Blog blog = null;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (blog == null)
+                        {
+                            blog = new Blog()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("BlogId")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                Url = reader.GetString(reader.GetOrdinal("Url")),
+                            };
+                        }
+
+
+                    }
+
+                    reader.Close();
+
+                    return blog;
+                }
+            }
+        }
+
+
+        public void Insert(Blog blog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Blog (Title, Url)
+                                            VALUES (@title, @url)";
+                    cmd.Parameters.AddWithValue("@title", blog.Title);
+                    cmd.Parameters.AddWithValue("@url", blog.Url);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+
+
+    }
+}
