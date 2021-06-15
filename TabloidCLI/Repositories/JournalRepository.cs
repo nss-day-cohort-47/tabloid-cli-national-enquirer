f0317cd0cfdf24b0c1e9939d7175778dd00a2719
Sidney Crandall <sdncrandall5@gmail.com>
2021-06-15 12:08:15 -0500

    JournalRepo started

---
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.Repositories
+{
+    public class JournalRepository : DataBaseConneector, IRepository<Journal>
+    {
+        public JournalRepository(string connectionString) : base(connectionString) { }
+
+        public List<Journal> GetAll()
+        {
+            using (SqlConnection conn = Connection))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd - conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Journal.Id,
+                                               Journal.Title,
+                                               Journal.Content,
+                                               Journal.CreateDateTime
+                                               FROM Journal";
+
+                    List<Journal> journals = new List<Journal>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Journal journal = new Journal()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Content = reader.GetString(reader.GetOridnal("Conetent")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                        };
+                        journals.Add(journal);
+                    }
+                    reader.Close();
+                    return journals;
+                }
+            }
+        }
+
+        public void Insert(Jounral journal)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Journal (Title, Conetnt, CreateDateTime)
+                                        VALUES (@title, @content, @createDateTime)";
+                }
+            }
+        }
+    }
+}
