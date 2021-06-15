3ad9f48ca571a3315b49a83627d4c490444b55b6
Laurel <lbentleym1992@gmail.com>
2021-06-15 16:39:58 -0400

    tags now able to edit

--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -53,9 +53,45 @@ namespace TabloidCLI.UserInterfaceManagers
             }
         }
 
+        private Tag Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose a Tag to update:";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Tag> tags = _tagRepository.GetAll();
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($" {i + 1}) {tag.Name}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return tags[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
+        }
+
+
         private void List()
         {
-            throw new NotImplementedException();
+            List<Tag> tags = _tagRepository.GetAll();
+            foreach (Tag tag in tags)
+            {
+                Console.WriteLine(tag);
+            }
         }
 
         private void Add()
@@ -66,18 +102,36 @@ namespace TabloidCLI.UserInterfaceManagers
             Console.Write("Tag name: ");
             tag.Name = Console.ReadLine();
 
+            Console.Clear();
             Console.WriteLine($"Adding {tag.Name} to tags.");
+
             _tagRepository.Insert(tag);
         }
 
         private void Edit()
         {
-            throw new NotImplementedException();
+           Tag tagToEdit = Choose("Which tag would you like to update? ");
+            if (tagToEdit == null)
+            {
+                return;
+            }
+            Console.WriteLine();
+            Console.Write("New name (blank to leave unchanged): ");
+                string name = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                tagToEdit.Name = name;
+            }
+            _tagRepository.Update(tagToEdit);
         }
 
         private void Remove()
         {
-            throw new NotImplementedException();
+            Tag tagToDelete = Choose("Which tag would you like to remove?");
+            if (tagToDelete != null)
+            {
+                _tagRepository.Delete(tagToDelete.Id);
+            }
         }
     }
 }
