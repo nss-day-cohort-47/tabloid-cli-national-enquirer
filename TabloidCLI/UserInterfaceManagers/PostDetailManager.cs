7dc108fe2af0bdecbc10dd043186d0a9a68ee8c3
Carter <carter.culkin@gmail.com>
2021-06-16 10:42:17 -0500

    Working now, a little rough around the edges

---
+++ TabloidCLI/UserInterfaceManagers/PostDetailManager.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    internal class PostDetailManager : IUserInterfaceManager
+    {
+        private IUserInterfaceManager _parentUI;
+        private PostRepository _postRepository;
+        private int _postId;
+
+        public PostDetailManager(IUserInterfaceManager parentUI, string connectionString, int postId)
+        {
+            _parentUI = parentUI;
+            _postRepository = new PostRepository(connectionString);
+            _postId = postId;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Post post = _postRepository.Get(_postId);
+            Console.WriteLine($"{post.Title} Details");
+            Console.WriteLine(" 1) View");
+            Console.WriteLine(" 2) Add Tag");
+            Console.WriteLine(" 3) Remove Tag");
+            Console.WriteLine(" 4) Note Management");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    View();
+                    return this;
+                case "2":
+                    AddTag();
+                    return this;
+                case "3":
+                    RemoveTag();
+                    return this;
+                case "4":
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void View()
+        {
+            Post post = _postRepository.Get(_postId);
+            Console.WriteLine($"Post title: {post.Title}");
+            Console.WriteLine($"Post URL: {post.Url}");
+            Console.WriteLine($"Post publication date: {post.PublishDateTime.ToString("D")}");
+            Console.WriteLine(@"
+                                        _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    Did you think you could escape me?
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        private void AddTag()
+        {
+            // not implemented
+
+        }
+        private void RemoveTag()
+        {
+            // not implemented 
+        }
+    }
+}
