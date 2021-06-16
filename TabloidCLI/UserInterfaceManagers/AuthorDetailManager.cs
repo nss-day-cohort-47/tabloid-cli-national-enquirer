9c31513f959a135a52581364840ec51b62a48541
Laurel <lbentleym1992@gmail.com>
2021-06-16 14:37:56 -0400

    author's blogs now showing

--- TabloidCLI/UserInterfaceManagers/AuthorDetailManager.cs
+++ TabloidCLI/UserInterfaceManagers/AuthorDetailManager.cs
@@ -74,7 +74,7 @@ namespace TabloidCLI.UserInterfaceManagers
             List<Post> posts = _postRepository.GetByAuthor(_authorId);
             foreach (Post post in posts)
             {
-                Console.WriteLine(post);
+                Console.WriteLine($"The author's post include: {post.Title}");
             }
             Console.WriteLine();
         }
